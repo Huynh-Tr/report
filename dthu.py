4eda07fb37052d9397bb50fd146db62ddcd1adc3
Huu Huynh Truong <truonghuynh210692@gmail.com>
2025-02-13 11:18:59 +0700

    update

--- dthu.py
+++ dthu.py
@@ -22,26 +22,24 @@ def dthu():
     # files_Dthu = glob(os.path.join(path, "Dthu\\*.csv"))
     # files_Dthu = load_github_files()
 
-    df = pd.concat([pd.read_parquet(file) for file in load_github_files()])
-    # df["Month year"] = pd.to_datetime(df["Month year"]).dt.to_period('M')
-    # lv2 = ["DÂY ĐỒNG HỒ", "MẮT KÍNH", "ĐỒNG HỒ"]
-    # lv3 = ["CAO"]
-    # lv4 = ["TRANH/TƯỢNG/BIỂU TƯỢNG", "VÀNG ÉP SIÊU PNJ", "VÀNG ÉP SIÊU SJC"]
-    # account_code = [51131000, 51132000]
-    # col = ["Product Group 2 Name", "Product Group 3 Name", "Product Group 4 Name", "Month year", \
-    #     "Plant Code", "Channel Description", "FM 01_Invoices Revenue", "FM 07_Gross Profit"]
+    df = pd.concat([pd.read_parquet(file) for file in load_github_files()]).dropna()
+    df["Month year"] = pd.to_datetime(df["Month year"]).dt.to_period('M')
+    lv2 = ["DÂY ĐỒNG HỒ", "MẮT KÍNH", "ĐỒNG HỒ"]
+    lv3 = ["CAO"]
+    lv4 = ["TRANH/TƯỢNG/BIỂU TƯỢNG", "VÀNG ÉP SIÊU PNJ", "VÀNG ÉP SIÊU SJC"]
+    account_code = [51131000, 51132000]
+    col = ["Product Group 2 Name", "Product Group 3 Name", "Product Group 4 Name", "Month year", \
+        "Plant Code", "Channel Description", "FM 01_Invoices Revenue", "FM 07_Gross Profit"]
 
-    # df_tsv = df.copy()
-    # df_tsv = df_tsv[~df_tsv["Product Group 2 Name"].isin(lv2)]
-    # df_tsv = df_tsv[~df_tsv["Product Group 3 Name"].isin(lv3)]
-    # df_tsv = df_tsv[~df_tsv["Product Group 4 Name"].isin(lv4)]
-    # df_tsv = df_tsv[~df_tsv["GL Account Code"].isin(account_code)]
-    # df_tsv = df_tsv[col]
+    df_tsv = df.copy()
+    df_tsv = df_tsv[~df_tsv["Product Group 2 Name"].isin(lv2)]
+    df_tsv = df_tsv[~df_tsv["Product Group 3 Name"].isin(lv3)]
+    df_tsv = df_tsv[~df_tsv["Product Group 4 Name"].isin(lv4)]
+    df_tsv = df_tsv[~df_tsv["GL Account Code"].isin(account_code)]
+    df_tsv = df_tsv[col]
 
-    # df_vm = df.copy()
-    # df_vm = df_vm[df_vm["Product Group 4 Name"].isin(lv4)]
-    # df_vm = df_vm[col]
+    df_vm = df.copy()
+    df_vm = df_vm[df_vm["Product Group 4 Name"].isin(lv4)]
+    df_vm = df_vm[col]
 
-    # return df_tsv, df_vm
-
-    return df.head() , df.tail()
+    return df_tsv, df_vm
