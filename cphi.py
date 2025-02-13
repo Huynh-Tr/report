324919de2426620f502ae09850c723fc3865885f
Huu Huynh Truong <truonghuynh210692@gmail.com>
2025-02-13 17:09:13 +0700

    update

--- cphi.py
+++ cphi.py
@@ -20,20 +20,20 @@ warnings.filterwarnings("ignore")
 cphi_parquet = r"https://raw.githubusercontent.com/Huynh-Tr/report/main/cphi.parquet"
 
 def cphi():        
-    df = pd.read_parquet(cphi_parquet)
-    df_cp["Month year"] = pd.to_datetime(df_cp["Month year"]).dt.to_period('M')
+    df = pd.read_parquet(dthu_parquet)
+    df["Month year"] = pd.to_datetime(df["Month year"]).dt.to_period('M')
 
     # create MaCH column with condition right 4 chartacters of cost center code if not null else right 4 characters of GL Account Code
-    df_cp["MaCH"] = df_cp["Cost Center Code"].fillna(df_cp["Plant Code"].astype('str')).astype('int').astype('str').str[-4:] \
+    df["MaCH"] = df["Cost Center Code"].fillna(df["Plant Code"].astype('str')).astype('int').astype('str').str[-4:] \
         .replace('0000', 'P015').replace('9999', 'P015')
 
-    df_cp["FM. Loc Amt"] = df_cp["FM. Loc Amt"].astype('float')
+    df["FM. Loc Amt"] = df["FM. Loc Amt"].astype('float')
 
     cols = ["Month year", "GL Account Code", "GL LV1 Name", "FM. Loc Amt", "PnL Name", "MaCH"]
     gl_code_exl = 64110120 # list of GL Account Code
     gl_code_range = [64100000, 64252100]
 
     # filter data cols with gl_code_exl and in range gl_code_range
-    df_cp = df_cp[cols][(df_cp["GL Account Code"] != gl_code_exl) & (df_cp["GL Account Code"].between(gl_code_range[0], gl_code_range[1]))]
+    df = df[cols][(df["GL Account Code"] != gl_code_exl) & (df["GL Account Code"].between(gl_code_range[0], gl_code_range[1]))]
 
-    return df_cp
+    return df
