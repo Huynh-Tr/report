d1951999269f42a5c1b32be55242cf10e1826d06
Huu Huynh Truong <truonghuynh210692@gmail.com>
2025-02-19 15:51:17 +0700

    update

--- dthu.py
+++ dthu.py
@@ -15,6 +15,9 @@ def dthu():
     df = df[df["Month year"] != "Month year"]
     df["Month year"] = pd.to_datetime(df["Month year"]).dt.to_period('M')
     df["Plant Code"] = df["Plant Code"].astype('str')
+    # replace any text start with "Trang Sức Ý *" with "Trang Sức Ý" in column Product Group 4 Name
+    df["Product Group 4 Name"] = df["Product Group 4 Name"].str.replace(r"^TRANG SỨC Ý.*", "TRANG SỨC Ý", regex=True)
+    df["Product Group 4 Name"] = df["Product Group 4 Name"].str.replace("TRANG SỨC", "TS")
     lv2 = ["DÂY ĐỒNG HỒ", "MẮT KÍNH", "ĐỒNG HỒ"]
     lv3 = ["CAO"]
     lv4 = ["TRANH/TƯỢNG/BIỂU TƯỢNG", "VÀNG KHÔNG ÉP SIÊU", "VÀNG ÉP SIÊU PNJ", "VÀNG ÉP SIÊU SJC"]
@@ -41,7 +44,6 @@ def chitieu_theoky(df, col, year, month, plant_code):
         chitieu_ck = df[(df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin([max(year) - 1]))][col].sum() / 1e9
         chitieu_lk = df[(df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin(year))][col].sum() / 1e9
         chitieu_ck_lk = df[(df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin([max(year) - 1]))][col].sum() / 1e9
-
     else:
         chitieu_th = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin(year))][col].sum() / 1e9
         chitieu_ck = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin([max(year) - 1]))][col].sum() / 1e9
@@ -78,7 +80,18 @@ def chitieu(year, month, plant_code='Select All'):
         }
     )    
 
-    return result_dthu
+    return result_dthu                  
 
-# result_dthu = chitieu(year=[2025], month=[1], plant_code='Select All')
-# st.write('Result DThu:', result_dthu)
+def chitieu_theoky_plot(df, year, month, plant_code='Select All'):
+    if plant_code == 'Select All':
+        chitieu_th = df[(df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin(year))]
+        chitieu_ck = df[(df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin([max(year) - 1]))]
+        chitieu_lk = df[(df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin(year))]
+        chitieu_ck_lk = df[(df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin([max(year) - 1]))]
+    else:
+        chitieu_th = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin(year))]
+        chitieu_ck = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(month)) & (df["Month year"].dt.year.isin([max(year) - 1]))]
+        chitieu_lk = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin(year))]
+        chitieu_ck_lk = df[(df["Plant Code"].isin(plant_code)) & (df["Month year"].dt.month.isin(range(1, max(month) + 1))) & (df["Month year"].dt.year.isin([max(year) - 1]))]
+
+    return chitieu_th, chitieu_ck, chitieu_lk, chitieu_ck_lk
