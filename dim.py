981e9d6b750bb47e5755c340616e0d35f99eddb8
Huu Huynh Truong <truonghuynh210692@gmail.com>
2025-02-24 11:47:12 +0700

    update

--- dim.py
+++ dim.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import streamlit as st 
 
-path_ch = r'https://raw.githubusercontent.com/Huynh-Tr/report/main/dims/Dim_DanhSach_CH.xlsx'
-path_gl = r'https://raw.githubusercontent.com/Huynh-Tr/report/main/dims/Dim_DanhMucTaiKhoan.xlsx'
 
 @st.cache_data
-def glaccount(path_gl):
-    df = pd.read_excel(linkgl)
+def glaccount():
+    path_gl = r'https://raw.githubusercontent.com/Huynh-Tr/report/main/dims/Dim_DanhMucTaiKhoan.xlsx'
+    df = pd.read_excel(path_gl)
     df["G/L Account"] = df["G/L Account"].astype(str)
     return df
 
 @st.cache_data
-def ten_ch(path_ch):
-    df = pd.read_excel(linkch, sheet_name="CuaHang (4cum)", header=2)
+def ten_ch():
+    path_ch = r'https://raw.githubusercontent.com/Huynh-Tr/report/main/dims/Dim_DanhSach_CH.xlsx'
+    df = pd.read_excel(path_ch, sheet_name="CuaHang (4cum)", header=2)
     df = df[["mã ch", "TÊN CỬA HÀNG", "AREA"]]
     # create new column Ma-Ten = mã ch - TÊN CỬA HÀNG
     df["Ma-Ten"] = "[" + df["mã ch"].astype('str') + " ] " + df["TÊN CỬA HÀNG"]
