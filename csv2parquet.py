3005a6c334ccb345a29ba6ae4741dc458f7fb43b
Huu Huynh Truong <truonghuynh210692@gmail.com>
2025-02-24 09:51:21 +0700

    update

--- csv2parquet.py
+++ csv2parquet.py
@@ -4,26 +4,17 @@ import pandas as pd
 from glob import glob
 import os
 
-output_path = "D:\pnj.com.vn\HuynhTN - Documents\Project\streamlit"
+output_path = r"D:\pnj.com.vn\HuynhTN - Documents\Project\streamlit"
 
-path = "D:\pnj.com.vn\HuynhTN - Documents\Data\DataBI"
-parquet_file = ['dthu.parquet', 'cphi.parquet', 'others.parquet', 'tonkho.parquet']
-csv_list_files = ([glob(os.path.join(path, "Dthu\\*.csv")),
-                  glob(os.path.join(path, "CP\\*.csv")),
-                  glob(os.path.join(path, "Others\\*.csv")),
-                  glob(os.path.join(path, "TK\\*.csv"))])
-parquet_file = [os.path.join(output_path, file) for file in parquet_file]
+def datamodelss(output_path):
+    path = r"D:\pnj.com.vn\HuynhTN - Documents\Data\DataBI"
+    parquet_file = ['dthu.parquet', 'cphi.parquet', 'others.parquet', 'tonkho.parquet']
+    csv_list_files = ([glob(os.path.join(path, "Dthu\\*.csv")),
+                    glob(os.path.join(path, "CP\\*.csv")),
+                    glob(os.path.join(path, "Others\\*.csv")),
+                    glob(os.path.join(path, "TK\\*.csv"))])
+    parquet_file = [os.path.join(output_path, file) for file in parquet_file]
 
-path_kh = "D:\pnj.com.vn\HuynhTN - Documents\Data\Planning"
-excel_list_files_kh = glob(os.path.join(path_kh, "*.xlsx"))
-parquet_file_kh = os.path.join(output_path, 'kh.parquet')
-
-path_03h = "D:\pnj.com.vn\HuynhTN - Documents\Data\SAP (FAGLL03H)"
-excel_list_files_03h = glob(os.path.join(path_03h, "*.xlsx"))
-parquet_file_03h = os.path.join(output_path, '03h.parquet')
-
-
-def datamodelss():
     for output_file, csv_files in zip(parquet_file, csv_list_files):
         if not os.path.exists(output_file):
             pd_df = pd.concat([pd.read_csv(csv_file) for csv_file in csv_files])
@@ -35,7 +26,10 @@ def datamodelss():
             table = pa.Table.from_pandas(pd_df)
             pq.write_table(table, output_file)
 
-def kh():
+def kh(output_path):
+    path_kh = r"D:\pnj.com.vn\HuynhTN - Documents\Data\Planning"
+    excel_list_files_kh = glob(os.path.join(path_kh, "*.xlsx"))
+    parquet_file_kh = os.path.join(output_path, 'kh.parquet')
     df = pd.DataFrame()
     for file in excel_list_files_kh:
         pd_df = pd.read_excel(file, sheet_name="Budget_Total", header=1)
@@ -58,28 +52,21 @@ def kh():
         table = pa.Table.from_pandas(df)
         pq.write_table(table, parquet_file_kh)
 
-# def fagll03h():
-#     df = pd.DataFrame()
-#     df = pd.concat([pd.read_excel(f) for f in files], ignore_index=True)
-#     df['Posting Date'] = pd.to_datetime(df['Posting Date']).dt.date
-#     df['Payment reference'] = df['Payment reference'].astype(str).str.replace('.0', '')
-#     df['Document Number'] = df['Document Number'].astype(str).str.replace('.0', '')
-#     df['Cost Center'] = df['Cost Center'].astype(str).str.replace('.0', '')
-#     df['Asset'] = df['Asset'].astype(str).str.replace('.0', '')
-#     cols = ["Company Code Currency Key", "Material", "Sales Document", "Billing Document", "Quantity", "Unit of Measure", "Order"]
-#     df = df.drop(columns=cols)
-
-def fagll03h():
+def fagll03h(output_path):
+    path_03h = r"D:\pnj.com.vn\HuynhTN - Documents\Data\SAP (FAGLL03H)"
+    excel_list_files_03h = glob(os.path.join(path_03h, "*.xlsx"))
+    parquet_file_03h = os.path.join(output_path, '03h.parquet')
     df = pd.concat([pd.read_excel(f) for f in excel_list_files_03h], ignore_index=True)
     df['Posting Date'] = pd.to_datetime(df['Posting Date']).dt.date
     df['Payment reference'] = df['Payment reference'].astype(str).str.replace('.0', '')
-    df['GL Account'] = df['GL Account'].astype(str).str.replace('.0', '')
+    df['G/L Account'] = df['G/L Account'].astype(str).str.replace('.0', '')
     df['Document Number'] = df['Document Number'].astype(str).str.replace('.0', '')
     df['Cost Center'] = df['Cost Center'].astype(str).str.replace('.0', '').str[-4:]
     df['Asset'] = df['Asset'].astype(str).str.replace('.0', '')
-    df['Cost center'] = df['Cost center'].astype(str).str.right(4)
     cols = ["Business Area", "Company Code Currency Key", "Material", "Sales Document", "Billing Document", "Quantity", "Unit of Measure", "Order"]
     df = df.drop(columns=cols)
+    df = df.rename(columns={"Company Code Currency Value": "Amt"})
+    df = df.rename(columns={"Text": "Description"})
 
     if not os.path.exists(parquet_file_03h):
         table = pa.Table.from_pandas(df)
@@ -92,7 +79,7 @@ def fagll03h():
 def main():
     # datamodelss()
     # kh()
-    fagll03h()
+    fagll03h(output_path)
 
 if __name__ == "__main__":
     main()
